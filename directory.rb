aa4309f1f5bb70b7740cfe2a0eb7ec387c34e0e6
Muna <mmhussein125@gmail.com>
2018-09-25 00:22:37 +0100

    Add print method grouping students by cohort

--- directory.rb
+++ directory.rb
@@ -11,7 +11,7 @@ def input_students
 
     while true do
       puts "Please enter cohort"
-      cohort = gets.chomp
+      cohort = gets.chomp.downcase.capitalize
       case cohort.downcase
       when "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"
         break
@@ -22,7 +22,7 @@ def input_students
         puts "Incorrect spelling"
       end
     end
-    students << {name: name, cohort: cohort.to_sym}
+    students << {name: name, cohort: cohort}
 
     puts "Now we have #{students.count} students"
     # get another name from the user
@@ -36,16 +36,35 @@ def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
+
 def print(students)
   students.each do |student|
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
+
+def print_cohort_groups(students)
+
+  sorted_by_cohort = {}
+
+  students.each do |student|
+  		cohort = student[:cohort]
+
+  		name = student[:name]
+
+  		if sorted_by_cohort[cohort] == nil
+  				sorted_by_cohort[cohort] = [name]
+  		else
+  				sorted_by_cohort[cohort].push(name)
+  		end
+  end
+  puts sorted_by_cohort.to_s.tr('{}"">[]', '').gsub!('=', ', ').split(',')
 end
+
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
 students = input_students
 print_header
-print(students)
+print_cohort_groups(students)
 print_footer(students)
