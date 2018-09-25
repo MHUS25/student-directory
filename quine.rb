732704a8d24019e88d9ddbc3d741cf7d96110f07
Muna <mmhussein125@gmail.com>
2018-09-26 00:25:59 +0100

    A program that prints its own source code

---
+++ quine.rb
@@ -0,0 +1,9 @@
+puts <<1*2,1
+puts <<1*2,1
+1
+
+# `puts << 1`: print everything after this line until you reach "1"
+# `*2`: print it twice
+# `,1`: and then print the value "1" (on a new line)
+# line 2 is the text to be printed
+# line 3 is the delimiter
